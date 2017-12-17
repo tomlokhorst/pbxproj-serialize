36d0fa2992b796a8c63edeb16a2cce60b730f1f5
Tom Lokhorst <tom@lokhorst.eu>
2017-12-17 18:00:59 +0100

    Update to XcodeEdit 2

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "pbxproj-serialize",
   dependencies: [
-    .Package(url: "https://github.com/tomlokhorst/XcodeEdit", majorVersion: 1),
+    .Package(url: "https://github.com/tomlokhorst/XcodeEdit", majorVersion: 2),
   ]
 )
